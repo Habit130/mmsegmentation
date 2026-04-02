12e5ffdede5a4b36b35aec78df589087c97689fc
Habit130 <xiguan130@gmail.com>
2026-04-02 08:04:49 +0800

    Increase IP102 training and eval batch sizes

--- configs/ip102/segformer_mit-b2_8xb2-160k_ip102-512x512.py
+++ configs/ip102/segformer_mit-b2_8xb2-160k_ip102-512x512.py
@@ -45,6 +45,6 @@ param_scheduler = [
         by_epoch=False)
 ]
 
-train_dataloader = dict(batch_size=16, num_workers=2)
-val_dataloader = dict(batch_size=4, num_workers=4)
+train_dataloader = dict(batch_size=24, num_workers=2)
+val_dataloader = dict(batch_size=8, num_workers=4)
 test_dataloader = val_dataloader
